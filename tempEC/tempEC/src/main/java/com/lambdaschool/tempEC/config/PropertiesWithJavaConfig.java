be9ebdf6f62326be9f21aa3332fcc575ace46f19
renegade27 <foemaster@live.com>
2019-07-10 22:32:18 -0500

    Profiles functionality added so we can use different property files between environments

---
+++ tempEC/tempEC/src/main/java/com/lambdaschool/tempEC/config/PropertiesWithJavaConfig.java
@@ -0,0 +1,9 @@
+//package com.lambdaschool.tempEC.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.PropertySource;
+//
+//@Configuration
+//@PropertySource({"classpath:persistence-${envTarget:mysql}.properties"})
+//public class PropertiesWithJavaConfig {
+//}
