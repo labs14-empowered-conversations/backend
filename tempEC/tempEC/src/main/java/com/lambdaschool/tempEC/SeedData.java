e44d07a00583018dbe80aca1622423343477dd89
omar okla <omaro@me.com>
2019-07-24 13:29:34 -0400

    Revert "Update Production to tested Staging changes"

--- tempEC/tempEC/src/main/java/com/lambdaschool/tempEC/SeedData.java
+++ tempEC/tempEC/src/main/java/com/lambdaschool/tempEC/SeedData.java
@@ -1,7 +1,10 @@
 package com.lambdaschool.tempEC;
 
-import com.lambdaschool.tempEC.models.Conversation;
-import com.lambdaschool.tempEC.services.ConversationService;
+import com.lambdaschool.tempEC.models.Role;
+import com.lambdaschool.tempEC.models.User;
+import com.lambdaschool.tempEC.models.UserRoles;
+import com.lambdaschool.tempEC.services.RoleService;
+import com.lambdaschool.tempEC.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -14,17 +17,58 @@ import java.util.ArrayList;
 public class SeedData implements CommandLineRunner
 {
     @Autowired
-    ConversationService convoService;
+    RoleService roleService;
+
+    @Autowired
+    UserService userService;
 
 
     @Override
     public void run(String[] args) throws Exception
     {
-        Conversation c1 = new Conversation("318-112-3827", "Jane", "318-321-4823");
-        convoService.save(c1);
+        Role r1 = new Role("admin");
+        Role r2 = new Role("user");
+        Role r3 = new Role("data");
 
-        Conversation c2 = new Conversation("318-421-5782", "Johnny", "281-382-2818");
-        convoService.save(c2);
+        roleService.save(r1);
+        roleService.save(r2);
+        roleService.save(r3);
 
+        // admin, data, user
+        ArrayList<UserRoles> admins = new ArrayList<>();
+        admins.add(new UserRoles(new User(), r1));
+        admins.add(new UserRoles(new User(), r2));
+        admins.add(new UserRoles(new User(), r3));
+        User u1 = new User("admin", "password", admins);
+        userService.save(u1);
+
+        // data, user
+        ArrayList<UserRoles> datas = new ArrayList<>();
+        datas.add(new UserRoles(new User(), r3));
+        datas.add(new UserRoles(new User(), r2));
+        User u2 = new User("labs1", "password", datas);
+        userService.save(u2);
+
+        // user
+        ArrayList<UserRoles> users = new ArrayList<>();
+        users.add(new UserRoles(new User(), r2));
+        User u3 = new User("labs2", "password", users);
+        userService.save(u3);
+
+        users = new ArrayList<>();
+        users.add(new UserRoles(new User(), r2));
+        User u4 = new User("labs3", "password", users);
+        userService.save(u4);
+
+        users = new ArrayList<>();
+        users.add(new UserRoles(new User(), r2));
+        User u5 = new User("labs4", "password", users);
+        userService.save(u5);
+
+        for(var i = 7; i <= 50; i++) {
+            users = new ArrayList<>();
+            users.add(new UserRoles(new User(), r2));
+            userService.save(new User("labs" + i, "password", users));
+        }
     }
 }
