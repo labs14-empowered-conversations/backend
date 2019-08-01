2f2836b4fc6fec2d2073008acb94015bd19479b2
renegade27 <foemaster@live.com>
2019-08-01 12:21:54 -0500

    text change

--- tempEC/src/main/java/com/lambdaschool/tempEC/controller/EmpConvoController.java
+++ tempEC/src/main/java/com/lambdaschool/tempEC/controller/EmpConvoController.java
@@ -60,7 +60,7 @@ public class EmpConvoController {
         List<NameValuePair> params = new ArrayList<NameValuePair>();
         params.add(new BasicNameValuePair("To", newConvo.getFfnumber()));
         params.add(new BasicNameValuePair("From", "+18476968785"));
-        params.add(new BasicNameValuePair("Body", "Hi " + newConvo.getFfname() + ", a friend or loved one is reaching out for support. Click here to help your friend: " + "https://empowered-conversation.netlify.com/conversation/resources/" + "?cid=" + textEncryptor.encrypt(Long.toString(createdConvo.getConversationid()))));
+        params.add(new BasicNameValuePair("Body", "Hi " + newConvo.getFfname() + ", a friend or loved one is reaching out for support. Click here to help them: " + "https://empowered-conversation.netlify.com/conversation/resources/" + "?cid=" + textEncryptor.encrypt(Long.toString(createdConvo.getConversationid()))));
         MessageFactory messageFactory = client.getAccount().getMessageFactory();
         try { messageFactory.create(params); } catch(Exception exc) { System.out.println(exc); };
         return new ResponseEntity<>(createdConvo, HttpStatus.CREATED);
